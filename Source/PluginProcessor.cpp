81f86fabf12612134b5d58c6a4abd0e6f381ccc8
Christopher Kalla <christopherkalla@macbookpro.lan>
2026-03-10 23:51:07 -0700

    adding category and mono

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -134,20 +134,14 @@ bool NebulaEQAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts)
     juce::ignoreUnused (layouts);
     return true;
   #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    // Effect: allow mono and stereo; require input and output layouts to match
+    auto out = layouts.getMainOutputChannelSet();
+    if (out != juce::AudioChannelSet::mono() && out != juce::AudioChannelSet::stereo())
         return false;
-
-    // This checks if the input layout matches the output layout
    #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+    if (layouts.getMainInputChannelSet() != out)
         return false;
    #endif
-
     return true;
   #endif
 }
